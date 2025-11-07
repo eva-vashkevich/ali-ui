bc380e12cacc0109838949ef3cdbf2b3bf98491b
Evgeniya Vashkevich <evgeniya.vashkevich@suse.com>
2025-11-06 16:56:58 -0800

    Revert "Cluster creation"

--- pkg/ali/provisioner.ts
+++ pkg/ali/provisioner.ts
@@ -1,14 +1,11 @@
 import { IClusterProvisioner, ClusterProvisionerContext } from '@shell/core/types';
 import { mapDriver } from '@shell/store/plugins';
 import { isProviderEnabled } from "@shell/utils/settings";
-import type { Component } from 'vue';
-import CruACK from "./components/CruACK.vue";
-import { isRancherPrime } from '@shell/config/version';
 export class ACKProvisioner implements IClusterProvisioner {
-  static ID = "alibaba";
+  static ID = 'alibaba'
 
   constructor(private context: ClusterProvisionerContext) {
-    mapDriver(this.id, "alibaba");
+    mapDriver(this.id, 'alibaba' );
   }
 
   get id(): string {
@@ -16,26 +13,23 @@ export class ACKProvisioner implements IClusterProvisioner {
   }
 
   get icon(): any {
-    return require("./icon.svg");
+    return require('./icon.svg');
   }
 
   get image(): any {
-    return require("./icon.svg");
+    return require('./icon.svg');
   }
 
   get group(): string {
-    return "hosted";
+    return 'hosted';
   }
 
   get label(): string {
-    return this.context.t("ali.label");
-  }
-  get component(): Component {
-    return CruACK;
+    return this.context.t('ali.label');
   }
 
   get hidden(): boolean {
-    return !isProviderEnabled(this.context, this.id) && isRancherPrime();
+    return !isProviderEnabled(this.context, this.id);
   }
   
   get prime(): boolean {
@@ -44,20 +38,17 @@ export class ACKProvisioner implements IClusterProvisioner {
 
   get detailTabs(): any {
     return {
-      machines: false,
-      logs: false,
+      machines:     false,
+      logs:         false,
       registration: false,
-      snapshots: false,
-      related: true,
-      events: false,
-      conditions: false,
+      snapshots:    false,
+      related:      true,
+      events:       false,
+      conditions:   false,
     };
   }
 
   get showImport(): boolean {
     return true;
-  }
-  get description(): string {
-    return this.context.t('ali.description');
   }
 }
