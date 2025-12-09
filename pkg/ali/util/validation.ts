4658840fb124ebd503d897fd7541c1c0d4cbaf67
Evgeniya Vashkevich <evgeniya.vashkevich@suse.com>
2025-12-09 15:05:58 -0800

    Added form validation (#55)

--- pkg/ali/util/validation.ts
+++ pkg/ali/util/validation.ts
@@ -1,53 +1,54 @@
 import ipaddr from "ipaddr.js";
-import { get } from '@shell/utils/object';
+import { get, set } from '@shell/utils/object';
+
+// Converts ip into 32 long integer by breaking it down into octets,
+// shifting existing value by 8 bits and adding new octet converted to base 10
+// In the end, we need to do a zero fill right shift to ensure final value is treated 
+// as unsigned 32-bit integer
 
 function ipToLong(ip: string): number {
   return (
-    ip.split(".").reduce((acc, octet) => (acc << 8) + parseInt(octet, 10), 0) >>> 0
+    ip.split(".").reduce((cur, octet) => (cur << 8) + parseInt(octet, 10), 0) >>> 0
   );
 }
 
 function getCidrRange(cidr: string): { start: number; end: number } | null {
-  const parts = cidr.split("/");
+  try {
+    const [ip, prefix] = ipaddr.parseCIDR(cidr);
 
-  if (parts.length !== 2) {
-    return null;
-  }
-
-  const [ip, prefString] = parts;
-  const prefix = parseInt(prefString, 10);
-
-  if (isNaN(prefix) || prefix < 0 || prefix > 32) {
-    return null; 
-  }
-  const ipParts = ip.split(".");
-
-  if (ipParts.length !== 4 ) {
+    if (ip.kind() !== 'ipv4') {
       return null;
+    }
+
+    const ipLong = ipToLong(ip.toString());
+    // Create a subnet mask
+    const mask = (0xffffffff << (32 - prefix)) >>> 0;
+    // Get a start of the ip range
+    const start = ipLong & mask;
+    // Get an end of the ip range
+    const end = start | (~mask >>> 0);
+
+    return { start, end };
+  } catch (e) {
+    //We can swallow this error
+    return null;  
   }
-  for( let i= 0 ; i< ipParts.length ; i++){
-    const part = ipParts[i];
-    if(isNaN(parseInt(part, 10)) ||
-      parseInt(part, 10) < 0 ||
-      parseInt(part, 10) > 255){
-        return null;
-      }
-  }
+}
 
-  const ipLong = ipToLong(ip);
-  const mask = (0xffffffff << (32 - prefix)) >>> 0;
+function nameLength(name: string): boolean {
+  return name.length <= 63;
+}
 
-  const start = ipLong & mask;
-  const end = start | (~mask >>> 0);
+function nameChars(name: string): boolean {   
+  return !!name.match(/^[\w-]+$/);
+}
 
-  return { start, end };
+function nameStart(name: string): boolean {
+  return !!name.match(/^[a-zA-Z0-9]/);
 }
 
 export function doCidrOverlap(cidr1: string, cidr2: string): boolean {
   if (!isValidCIDR(cidr1) || !isValidCIDR(cidr2)) {
-    return false;
-  }
-  if (!cidr1 || !cidr2) {
     return false;
   }
   const range1 = getCidrRange(cidr1);
@@ -79,16 +80,15 @@ export const requiredInCluster = (ctx: any, labelKey: string, clusterPath: strin
 export const clusterNameChars = (ctx: any ) => {
   return () :string | undefined => {
     const { name = '' } = get(ctx, 'normanCluster');
-    const nameIsValid = name.match(/^[\w-]+$/);
 
-    return !needsValidation(ctx) || nameIsValid ? undefined : ctx.t('validation.clusterName.clusterNameChars');
+    return !needsValidation(ctx) || nameChars(name) ? undefined : ctx.t('validation.clusterName.clusterNameChars');
   };
 };
 
 export const clusterNameStart = (ctx: any) => {
   return () :string | undefined => {
     const { name = '' } = get(ctx, 'normanCluster');
-    const nameIsValid = (!!name.match(/^[a-zA-Z0-9]/) || !name.length);
+    const nameIsValid = (nameStart(name) || !name.length);
 
     return !needsValidation(ctx) || nameIsValid ? undefined : ctx.t('validation.clusterName.clusterNameStart');
   };
@@ -97,8 +97,102 @@ export const clusterNameStart = (ctx: any) => {
 export const clusterNameLength = (ctx: any) => {
   return () : string | undefined => {
     const { clusterName = '' } = get(ctx, 'config');
-    const isValid = clusterName.length <= 63;
+    const isValid = nameLength(clusterName);
     // The at least 1 case is covered by a separate check
     return isValid ? undefined : ctx.t('validation.clusterName.length');
   };
+};
+
+export const nodePoolNames = (ctx: any) => {
+  return (poolName:string) :string | undefined => {
+    let allAvailable = true;
+
+    const isValid = (name:string) =>nameLength(name) && nameChars(name) && nameStart(name);
+
+    if (poolName || poolName === '') {
+      return isValid(poolName) ? undefined : ctx.t('validation.poolName');
+    } else {
+      ctx.nodePools.forEach((pool: any) => {
+        const name = pool.name || '';
+
+        if (!isValid(name)) {
+          set(pool._validation, '_validName', false);
+
+          allAvailable = false;
+        } else {
+          set(pool._validation, '_validName', true);
+        }
+      });
+      if (!allAvailable) {
+        return ctx.t('validation.poolName');
+      }
+    }
+  };
+};
+
+export const nodePoolNamesUnique = (ctx: any) => {
+  return () :string | undefined => {
+    const poolNames = (ctx.nodePools || []).map((pool: any) => pool.name);
+
+    const hasDuplicates = poolNames.some((name: string, idx: number) => poolNames.indexOf(name) !== idx);
+
+    if (hasDuplicates) {
+      return ctx.t('validation.poolNamesUnique');
+    }
+  };
+};
+
+export const nodePoolCount = (ctx:any) => {
+  return (desiredSize?: number, _isNew = false) => {
+    const config = get(ctx, 'config');
+    const type = config.clusterSpec;
+    
+    const isBasic = type === 'ack.standard';
+    let errMsg = !isBasic? ctx.t('validation.nodeCountPro'): ctx.t('validation.nodeCountBasic');
+    const min = 0;
+
+    if (desiredSize || desiredSize === 0) {
+      const max = !_isNew ? 500 : ( isBasic ? 10 : 5000 );
+      return desiredSize >= min && desiredSize <= max ? undefined : errMsg;
+    } else {
+      let allValid = true;
+
+      ctx.nodePools.forEach((pool: any) => {
+        const { desiredSize = 0, _isNew } = pool;
+        
+        const max = !_isNew ? 500 : ( isBasic ? 10 : 5000 );
+
+        if (desiredSize < min || desiredSize > max) {
+          pool._validation['_validCount'] = false;
+          allValid = false;
+        } else {
+          pool._validation['_validCount'] = true;
+        }
+      });
+
+      return allValid ? undefined : errMsg;
+    }
+  };
+};
+
+export const instanceTypeCount = (ctx:any) => {
+  return (instanceTypes?: Array<string>) => {  
+    if(instanceTypes){
+      return instanceTypes.length > 0 && instanceTypes.length <= 20 ? undefined : ctx.t('validation.instanceTypeCount');
+    } else {
+      let allValid = true;
+
+      ctx.nodePools.forEach((pool: any) => {
+        const { instanceTypes = [] } = pool;
+        if(!(instanceTypes.length > 0 && instanceTypes.length <= 20)){
+          pool._validation['_validInstanceTypeCount'] = false;
+          allValid = false;
+        } else {
+          pool._validation['_validInstanceTypeCount'] = true;
+        }
+      });
+
+      return allValid ? undefined : ctx.t('validation.instanceTypeCount');
+    }
+  }
 };
